fefeb9ea53e6377c2c330c56b6eb6d249e1389bc
Your Name <you@example.com>
2021-05-08 22:23:44 +0200

    refactor: add parameters to an input

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -7,7 +7,7 @@ ui <- dashboardPage(
   dashboardSidebar(disable = TRUE),
   dashboardBody(
     box(title = "Import your data", status = "primary",
-        fileInput("fileToRead", "Select your file")
+        fileInput("fileToRead", "Select your file", multiple = FALSE)
         ),
     box(title = "Your data imported"),
     box(title = "Convert your data")
