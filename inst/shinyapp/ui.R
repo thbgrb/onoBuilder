605ec0ca11aa937d20ddaa9a2774999b3d66bed1
Your Name <you@example.com>
2021-05-08 22:11:09 +0200

    style: add an input box

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -1,10 +1,14 @@
 ## ui of the shiny app
 
+library(shinydashboard)
+
 ui <- dashboardPage(
   dashboardHeader(title = "The Observer Converter"),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
-    box(title = "Import your data", status = "primary"),
+    box(title = "Import your data", status = "primary",
+        fileInput("fileToRead", "Select your file")
+        ),
     box(title = "Your data imported"),
     box(title = "Convert your data")
   )
